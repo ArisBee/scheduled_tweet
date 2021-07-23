946e96c88ac0b72aac9f80c33d25378c15d62a49
ArisBee <aristide.bouix@gmail.com>
2021-07-23 16:38:42 +0200

    Upgraded the CI script

--- scripts/setLicense.sh
+++ scripts/setLicense.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # declare an array variable listing all the allowed license
-declare -a licenses_list=(MIT ISC "Apache 2.0" "Simplified BSD" "New BSD" WTFPL "(MIT AND BSD-3-Clause)" "Simplified BSD ruby" CC0-1.0)
+declare -a licenses_list=(MIT ISC "Apache 2.0" "Simplified BSD" "New BSD" WTFPL "(MIT AND BSD-3-Clause)" "Simplified BSD ruby" CC0-1.0 "Brakeman Public Use License")
 
 ## enable licenses one by one in a loop
 for license in "${licenses_list[@]}"; do
