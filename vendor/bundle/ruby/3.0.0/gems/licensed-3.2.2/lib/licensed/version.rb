a73a70ae7be19664594f2f6fe286579b0b0ecca4
ArisBee <a.bouix@catawiki.nl>
2021-11-30 22:24:48 +0100

    Fixed dependencies

--- vendor/bundle/ruby/3.0.0/gems/licensed-3.2.2/lib/licensed/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module Licensed
-  VERSION = "3.2.2".freeze
-
-  def self.previous_major_versions
-    major_version = Gem::Version.new(Licensed::VERSION).segments.first
-    (1...major_version).to_a
-  end
-end
